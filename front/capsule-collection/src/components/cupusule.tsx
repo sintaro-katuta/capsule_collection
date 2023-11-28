e3ac813ee74cdbc6062e6612f596090d4f2d3671
sintaro-katuta <sintaro.katuta@gmail.com>
2023-11-28 12:56:40 +0900

    feat: 検索画面完成

--- front/capsule-collection/src/components/cupusule.tsx
+++ front/capsule-collection/src/components/cupusule.tsx
@@ -10,7 +10,7 @@ export default function Cupusule(props: any) {
                 </div>
                 <p className="text-background">ちぃかわ</p>
             </div>
-            <div className="flex justify-center items-center w-full h-3/5 px-4 overflow-x-auto">
+            <div className="flex justify-center items-center w-full h-3/5 px-4 overflow-x-auto hide-scroll-bar">
                 {props.capsule.map((cp: any, i: number) => (
                     <div className="relative w-16 h-16 mx-2 border-2 border-black rounded-full bg-background flex-none hide-scroll-bar" key={i}>
                         <Image src={cp.image} layout="fill" objectFit="contain" alt="Icon" className="object-contain rounded-full p-1" />
