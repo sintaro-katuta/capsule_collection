9c8cce4f61f45fb9af2d0adb43911119d7cb7e14
sintaro-katuta <sintaro.katuta@gmail.com>
2024-01-30 16:24:20 +0900

    fix: カメラサイズ再調整

--- front/capsule-collection/src/components/qr_camera.tsx
+++ front/capsule-collection/src/components/qr_camera.tsx
@@ -20,8 +20,8 @@ export default function Qr_Camera(props: any){
     };
 
     useEffect(() => {
-        let contentWidth = 320
-        let contentHeight = 480
+        let contentWidth = 720
+        let contentHeight = 1280
         const config = { audio:false, video: { facingMode: "environment", width: contentWidth, height: contentHeight }}
 
         const ctx = canvasRef.current?.getContext('2d')
@@ -85,7 +85,7 @@ export default function Qr_Camera(props: any){
                 alt=""
                 onClick={(e: React.FormEvent) => cancel(e)}
             />
-            <video ref={videoRef} autoPlay playsInline width={320} height={480} className={`flex justify-center ${cameraSwitch}`}></video>
+            <video ref={videoRef} autoPlay playsInline width={720} height={1280} className={`flex justify-center ${cameraSwitch}`}></video>
             <canvas ref={canvasRef} className='hidden'></canvas>
             {qrCode &&
                 <>
